53d62fa0674387c5d2f62b7dfb1cc377b13b789e
Watanabe Toshinori <toshinori_watanabe@tiny.blue>
2016-04-12 23:31:00 +0900

    update priffy function

--- pritty_function.swift
+++ pritty_function.swift
@@ -4,4 +4,4 @@ summary: "__PRITTY_FUNCTION__ alternative for Swift."
 completion-scope: All
 ---
 
-"\(NSDate()) [\(self.dynamicType) \(__FUNCTION__)] [Line \(__LINE__)]"
+"\(NSDate()) [\(self.dynamicType) \(#function)] [Line \(#line)]"
