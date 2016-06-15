6ecaad8784c71535b0c89342dd298d83bfcbc9e8
Watanabe Toshinori <toshinori_watanabe@tiny.blue>
2016-06-14 20:25:18 -0700

    update pritty_function snippet

--- pritty_function.swift
+++ pritty_function.swift
@@ -4,4 +4,4 @@ summary: "__PRITTY_FUNCTION__ alternative for Swift."
 completion-scope: All
 ---
 
-"\(NSDate()) [\(self.dynamicType) \(#function)] [Line \(#line)]"
+"\(Date()) [\(self.dynamicType) \(#function)] [Line \(#line)]"
