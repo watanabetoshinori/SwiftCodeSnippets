1e0c8330d1715eafe1cc60939da13b041cd892a6
Watanabe Toshinori <toshinori_watanabe@tiny.blue>
2016-06-14 20:24:43 -0700

    update load_xib snippet

--- load_xib.swift
+++ load_xib.swift
@@ -5,13 +5,12 @@ completion-scope: CodeExpression
 ---
 
 // TODO: Open the xib file and set this class as 'File's owner'.
-
-let bundle = NSBundle(forClass: self.dynamicType)
+let bundle = Bundle(for: self.dynamicType)
 let nib = UINib(nibName: <#xib name#>, bundle: bundle)
-let view = nib.instantiateWithOwner(self, options: nil).first as! UIView
+let view = nib.instantiate(withOwner: self, options: nil).first as! UIView
 
 addSubview(view)
  
 view.translatesAutoresizingMaskIntoConstraints = false
-addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-(0)-[view]-(0)-|", options: [.AlignAllCenterY], metrics: nil, views: ["view": view]))
-addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-(0)-[view]-(0)-|", options: [.AlignAllCenterX], metrics: nil, views: ["view": view]))
+addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-(0)-[view]-(0)-|", options: [.alignAllCenterY], metrics: nil, views: ["view": view]))
+addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-(0)-[view]-(0)-|", options: [.alignAllCenterX], metrics: nil, views: ["view": view]))
