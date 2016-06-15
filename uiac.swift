5a1f0432febff62e79cd9b9c73aee411fafe49b9
Watanabe Toshinori <toshinori_watanabe@tiny.blue>
2016-06-14 20:25:26 -0700

    update uiac snippet

--- uiac.swift
+++ uiac.swift
@@ -4,8 +4,8 @@ summary: "UIAlertController template for Swift."
 completion-scope: CodeExpression
 ---
 
-let alertController = UIAlertController(title: <#T##String?#>, message: <#T##String?#>, preferredStyle: .Alert)
-alertController.addAction(UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
+let alertController = UIAlertController(title: <#T##String?#>, message: <#T##String?#>, preferredStyle: .alert)
+alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
     <#code#>
 }))
-alertController.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
