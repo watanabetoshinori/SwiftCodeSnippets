97af26c07ea95571c630447bc0bfc2587d4b4dcf
Watanabe Toshinori <toshinori_watanabe@tiny.blue>
2016-06-14 20:24:28 -0700

    update log snippet

--- log.swift
+++ log.swift
@@ -4,4 +4,4 @@ summary: "Basic print log snippet for Swift."
 completion-scope: All
 ---
 
-print("\(NSDate()) [\((#file as NSString).lastPathComponent):\(#line) \(#function)]")
+print("\(Date()) [\((#file as NSString).lastPathComponent):\(#line) \(#function)]")
