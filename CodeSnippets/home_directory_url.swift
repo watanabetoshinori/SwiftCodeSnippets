cb2f5f8d99202efca9324e8721ba7f249e37be7a
Watanabe Toshinori <toshinori_watanabe@tiny.blue>
2018-11-29 23:37:48 +0900

    Update project structure

---
+++ CodeSnippets/home_directory_url.swift
@@ -0,0 +1,7 @@
+---
+title: "Swift Home Directory URL"
+summary: "Home directory url."
+completion-scope: All
+---
+
+let homeDirectoryURL = URL(fileURLWithPath: NSHomeDirectory())
--- install_all.py
+++ install_all.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 
-for root, dirs, files in os.walk("."):
+for root, dirs, files in os.walk("./CodeSnippets"):
 	for file in files:
 		if file.endswith('.swift'):
 			cmd = "xcodesnippet install %s" % file
